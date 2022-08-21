59a45c84f3912c8f95e162c157d5fd1b700b6c76
ALASHI1 <alashimuyiwa@gmail.com>
2022-08-21 03:40:56 +0100

    heroku

--- qr_generator/settings.py
+++ qr_generator/settings.py
@@ -149,16 +149,6 @@ WSGI_APPLICATION = "qr_generator.wsgi.application"
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         "ENGINE": 'django.db.backends.postgresql',
-#         "NAME": env("DB_NAME"),
-#         "USER": env("DB_USER"),
-#         "PASSWORD": env("DB_PASS"),
-#         "HOST": env("DB_HOST"),
-#         "PORT": env("DB_PORT"),
-#     }
-#     }
 DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
 
 # Password validation
