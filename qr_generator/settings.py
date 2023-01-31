3d1a7dbf81cfa2ec06fffd7372370a414910e9fe
ALASHI1 <115797122+Muyiwa0@users.noreply.github.com>
2023-01-31 19:28:15 +0100

    vercel

--- qr_generator/settings.py
+++ qr_generator/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = "django-insecure-)0b7sbcc_9kl+cg@wda@5k-+arj*zk0!&qof1b-$juqw!vp&48"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
